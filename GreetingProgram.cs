e7925e67952c3de09dc9801db5db398b4021ae9f
VoltaireGit <voltairearvinuy2004@gmail.com>
2024-08-20 20:39:04 +0800

    Add files via upload

---
+++ GreetingProgram.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+public class GreetingProgram
+{
+	public Class1()
+	{
+        Console.WriteLine("Enter your name: ");
+        String name = Console.ReadLine();
+        Console.WriteLine("Enter the total number of your enrolled courses: ");
+        int courses = Console.Read();
+        Console.WriteLine("Enter the price of your favourite book: ");
+        int price = Console.Read();
+
+        Console.WriteLine(" ");
+        Console.WriteLine("Name: " + name);
+        Console.WriteLine("Total enrolled courses: " + courses);
+        Console.WriteLine("price of my favourite book: " + price);
+    }
+}
